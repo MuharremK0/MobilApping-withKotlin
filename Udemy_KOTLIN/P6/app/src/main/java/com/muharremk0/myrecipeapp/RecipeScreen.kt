af87323f0d17bbdd4420ed8ca6d85a7a92ffde5f
Muharrem <ozcankocabiyik07@gmail.com>
2024-05-12 23:36:45 +0300

    2.1
    new specifacation are added.

--- Udemy_KOTLIN/P6/app/src/main/java/com/muharremk0/myrecipeapp/RecipeScreen.kt
+++ Udemy_KOTLIN/P6/app/src/main/java/com/muharremk0/myrecipeapp/RecipeScreen.kt
@@ -1,6 +1,7 @@
 package com.muharremk0.myrecipeapp
 
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.aspectRatio
@@ -24,9 +25,9 @@ import androidx.lifecycle.viewmodel.compose.viewModel
 import coil.compose.rememberAsyncImagePainter
 
 @Composable
-fun RecipeScreen(modifier : Modifier = Modifier){
-    val recipeViewModel : MainViewModel = viewModel()
-    val viewstate by recipeViewModel.categoriesState
+fun RecipeScreen(modifier : Modifier = Modifier,
+                 viewstate:MainViewModel.RecipeState,
+                 navigateToDetail: (Category) -> Unit){
 
     Box(modifier = Modifier.fillMaxSize()){
         when{
@@ -39,7 +40,7 @@ fun RecipeScreen(modifier : Modifier = Modifier){
             }
             else ->{
                 //Display Categories
-                CategoryScreen(categories = viewstate.list)
+                CategoryScreen(categories = viewstate.list,navigateToDetail)
             }
         }
     }
@@ -48,20 +49,21 @@ fun RecipeScreen(modifier : Modifier = Modifier){
 }
 
 @Composable
-fun CategoryScreen(categories : List<Category>){
+fun CategoryScreen(categories: List<Category>, navigateToDetail: (Category) -> Unit){
     LazyVerticalGrid(GridCells.Fixed(2),modifier = Modifier.fillMaxSize()) {
         items(categories){
-            category -> CategoryItem(category = category)
+            category -> CategoryItem(category = category,navigateToDetail)
         }
     }
 }
 
 //How each Items looks like
 @Composable
-fun CategoryItem(category : Category){
+fun CategoryItem(category : Category,navigateToDetail: (Category) -> Unit){
     Column(modifier = Modifier
         .padding(8.dp)
-        .fillMaxSize(),
+        .fillMaxSize()
+        .clickable {navigateToDetail(category)},
         horizontalAlignment = Alignment.CenterHorizontally)
     {
         Image(
